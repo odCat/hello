78ed58e8ffd351000a0a773f6bffbeabb1a511cf
Mihai Gătejescu <gus666xe@gmail.com>
2018-10-31 12:41:32 +0200

    Set the i18n environment

--- hello.c
+++ hello.c
@@ -2,6 +2,10 @@
 
 int main (void)
 {
+    setlocale (LC_ALL, "");
+    bindtextdomain ("hello", getenv("PWD"));
+    textdomain ("hello");
+
     printf (_("Hello, World!\n"));
 
     return 0;
--- hello.h
+++ hello.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "gettext.h"
+#include "locale.h"
 
 #define _(String) gettext (String)
