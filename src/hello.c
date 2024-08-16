a9528b724beaa7dc48ec87ffd46c5ad99b232209
Mihai Gătejescu <gus666xe@gmail.com>
2024-08-16 22:19:59 +0300

    Use the library instead of the project header file

--- src/hello.c
+++ src/hello.c
@@ -3,10 +3,8 @@
 int main(void)
 {
     setlocale(LC_ALL, "");
-    bindtextdomain("hello", "/usr/local/share/locale/");
+    bindtextdomain("hello", "/usr/local/share/locale");
     textdomain("hello");
 
     printf(_("Hello, world!\n"));
-
-    return 0;
 }
